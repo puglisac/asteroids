1a0ecd940f37f7310a11ecdfc260952639ab2c3e
puglisac <alan.c.puglisi@gmail.com>
2021-01-04 13:59:37 -0600

    added screen wrapping

---
+++ Assets/Scripts/ClampInScreen.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A character
+/// </summary>
+public class ClampInScreen : MonoBehaviour
+{
+    // saved for efficiency
+    float shipColliderHalfLength;
+    float shipColliderHalfHeight;
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+	{
+        // save for efficiency
+        BoxCollider2D collider = gameObject.GetComponent<BoxCollider2D>();
+        shipColliderHalfLength = collider.size.x / 2;
+        shipColliderHalfHeight = collider.size.x / 2;
+
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+	{
+        Clamp();
+	}
+
+    /// <summary>
+    /// Clamps the character in the screen
+    /// </summary>
+    void Clamp()
+    {
+        // clamp position as necessary
+        Vector3 position = transform.position;
+        if (position.x - shipColliderHalfLength > ScreenUtils.ScreenRight)
+        {
+            position.x = ScreenUtils.ScreenLeft - shipColliderHalfLength;
+        }
+        if (position.x + shipColliderHalfLength < ScreenUtils.ScreenLeft)
+        {
+            position.x = ScreenUtils.ScreenLeft + shipColliderHalfLength;
+        }
+        if (position.y - shipColliderHalfHeight > ScreenUtils.ScreenTop)
+        {
+            position.y = ScreenUtils.ScreenBottom - shipColliderHalfHeight;
+        }
+        if (position.y + shipColliderHalfHeight < ScreenUtils.ScreenBottom)
+        {
+            position.y = ScreenUtils.ScreenTop + shipColliderHalfHeight;
+        }
+        transform.position = position;
+    }
+}
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -6,20 +6,15 @@ public class Ship : MonoBehaviour
 {
 
     Rigidbody2D shipRigidBody;
-    Vector2 thrustDirection = new Vector2(1,0);
-    const float ThrustForce = 0.8f;
 
-    void FixedUpdate()
-    {
-        if (Input.GetAxis("Thrust") > 0)
-        {
-            shipRigidBody.AddForce(thrustDirection * ThrustForce, ForceMode2D.Force);
-        }
-    }
+    Vector2 thrustDirection = new Vector2(1,0);
+
+    const float ThrustForce = 0.8f;
 
     // Start is called before the first frame update
     void Start()
     {
+        //set the shipRigidBody field to the Rigidbody2D of the game object
         shipRigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
@@ -28,4 +23,19 @@ public class Ship : MonoBehaviour
     {
 
     }
+
+    void FixedUpdate()
+    {
+        //adds thrust to the game objects rigidbody when 'Thrust' axis > 0
+        if (Input.GetAxis("Thrust") > 0)
+        {
+            shipRigidBody.AddForce(thrustDirection * ThrustForce, ForceMode2D.Force);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        
+    }
 }
+
