61d6ea2e100d1b6b05fdf165ee2dfff2a588f820
puglisac <alan.c.puglisi@gmail.com>
2021-01-04 13:15:34 -0600

    added ship script and thrust code

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+
+    Rigidbody2D shipRigidBody;
+    Vector2 thrustDirection = new Vector2(1,0);
+    const float ThrustForce = 0.8f;
+
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Thrust") > 0)
+        {
+            shipRigidBody.AddForce(thrustDirection * ThrustForce, ForceMode2D.Force);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipRigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
