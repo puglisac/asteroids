8d8e51b01429957047938cec89997cddf7856f46
puglisac <alan.c.puglisi@gmail.com>
2021-01-09 13:15:07 -0600

    added collision free spawing

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -12,17 +12,43 @@ public class AsteroidSpawner : MonoBehaviour
 
     Timer spawnTimer;
 
+    // collision-free spawn support
+    const int MaxSpawnTries = 20;
+    float asteroidrColliderHalfWidth;
+    float asteroidColliderHalfHeight;
+    Vector2 min = new Vector2();
+    Vector2 max = new Vector2();
+
+    //spawn location support
+    Vector3 location = new Vector3();
+    float minSpawnX;
+    float maxSpawnX;
+    float minSpawnY;
+    float maxSpawnY;
+
+
     /// <summary>
     /// Use this for initialization
     /// </summary>
     void Start()
     {
 
+        minSpawnX = ScreenUtils.ScreenLeft;
+        maxSpawnX = ScreenUtils.ScreenRight;
+        minSpawnY = ScreenUtils.ScreenBottom;
+        maxSpawnY = ScreenUtils.ScreenTop;
 
         // create and start timer
         spawnTimer = gameObject.AddComponent<Timer>();
         spawnTimer.Duration = 1;
         spawnTimer.Run();
+
+        // spawn and destroy an asteroid to cache collider values
+        GameObject tempRock = Instantiate(prefabRock) as GameObject;
+        BoxCollider2D collider = tempRock.GetComponent<BoxCollider2D>();
+        asteroidrColliderHalfWidth = collider.size.x / 2;
+        asteroidColliderHalfHeight = collider.size.y / 2;
+        Destroy(tempRock);
     }
 
     /// <summary>
@@ -49,10 +75,36 @@ public class AsteroidSpawner : MonoBehaviour
     /// </summary>
     void SpawnRock()
     {
-        Vector3 location = new Vector3(Random.Range(ScreenUtils.screenLeft, ScreenUtils.screenRight),
-                    Random.Range(ScreenUtils.screenBottom, ScreenUtils.screenTop),
-                    -Camera.main.transform.position.z);
-        GameObject rock = Instantiate(prefabRock) as GameObject;
-        rock.transform.position = location;
+        location.x = Random.Range(minSpawnX, maxSpawnX);
+        location.y = Random.Range(minSpawnY, maxSpawnY);
+        location.z = -Camera.main.transform.position.z;
+
+        SetMinAndMax(location);
+
+        int spawnTries = 1;
+        while (Physics2D.OverlapArea(min, max) != null &&
+               spawnTries < MaxSpawnTries)
+        {
+            // change location and calculate new rectangle points
+            location.x = Random.Range(minSpawnX, maxSpawnX);
+            location.y = Random.Range(minSpawnY, maxSpawnY);
+            SetMinAndMax(location);
+
+            spawnTries++;
+        }
+
+        if (Physics2D.OverlapArea(min, max) == null)
+        {
+            GameObject rock = Instantiate(prefabRock) as GameObject;
+            rock.transform.position = location;
+        }
+    }
+
+    void SetMinAndMax(Vector3 location)
+    {
+        min.x = location.x - asteroidrColliderHalfWidth;
+        min.y = location.y - asteroidColliderHalfHeight;
+        max.x = location.x + asteroidrColliderHalfWidth;
+        max.y = location.y + asteroidColliderHalfHeight;
     }
 }
